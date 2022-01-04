a249fa91f49db384cd593fa817a6a550b318a3ba
skyend3 <94793732+Skyend3@users.noreply.github.com>
2022-01-04 19:47:08 +0100

    Add files via upload

---
+++ calculeptsbac.py
@@ -0,0 +1,2 @@
+#Voici la page sur laquelle nous ferons le projet qui je le rappelle est "calculer les points du bac".
+#Merci de mettre des noms compréhensible que l'on ait pas à chercher trop longtemps.
