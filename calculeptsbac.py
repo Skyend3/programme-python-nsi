1314c27db8ad8242dc5214b29416074148c069df
skyend3 <94793732+Skyend3@users.noreply.github.com>
2022-01-04 19:50:37 +0100

    Update calculeptsbac.py

--- calculeptsbac.py
+++ calculeptsbac.py
@@ -1,4 +1,3 @@
 #Voici la page sur laquelle nous ferons le projet qui je le rappelle est "calculer les points du bac".
 #Merci de mettre des noms compréhensible que l'on ait pas à chercher trop longtemps.
 typebac=input("Quel est votre série de baccalauréa")
-print(typebac)
