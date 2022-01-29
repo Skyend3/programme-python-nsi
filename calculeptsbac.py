4621513324ca19eb046c86cc413bbd6d98dcc7c8
max1me <94793732+Skyend3@users.noreply.github.com>
2022-01-29 19:56:00 +0100

    Update calculeptsbac.py

--- calculeptsbac.py
+++ calculeptsbac.py
@@ -1,16 +1,21 @@
-#demande de série
+# demande de série
+
 serie = ()
-while len(serie) == 0 or serie.upper()!="générale" or "generale" or "général" or "general":
-    serie = str(input("De quel série faites vous parties générale ou technologique. "))
+fin = False
 
-#série générale
-    if serie.upper() == "générale" or "generale" or "général" or "general":
-        print("Vous avez choisis la voie générale")
-        break
-#série technologique   
-    elif serie.upper() == "technologique" :
+while not fin:
+    serie = str(input("De quel série faites vous parties générale ou technologique. ")).replace("é", "e")
+    
+    # série générale
+    if serie.upper() == "GENERAL" or serie.upper() == "GENERALE":
+        print("Vous êtes dans la série générale.")
+        fin = True
+
+    # série technologique
+    elif serie.upper() == "TECHNOLOGIE" or serie.upper() == "TECHNOLOGIQUE" or serie.upper() == "TECHNO":
         print("Vous êtes dans la série technologique.")
+        fin = True
 
-        break
+    # si on se trompe
     else :
-        print("Veuillez écrire soit : générale pour la voix général soit technologique pour la voix technologique.")
+        print("Veuillez écrire soit : générale ou technologique.")
